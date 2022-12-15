c2a6edf2f71f75f6835cf694b29dd68a0a75d4b4
Mishachkld <mishachkld@gmail.com>
2022-12-15 23:49:54 +0200

    исправленно описание

--- main.cpp
+++ main.cpp
@@ -4,15 +4,9 @@
 
 #define SIZE 6
 
-//     x |= (1 << n); //записать 1 в бит Н
-//     x &= ~(1 << n); //записать 0
-//     if (x & (1 << 2)) {  /* во второй бит вписана единица */ }
-
 void outputArray(std::vector<char> array);
-//std::cout << (int) number[i];
 
-
-std::vector<char> to2(int number) {   /// char ['0','0','0','0','0','0','0','0']  // перевод в 2-ную
+std::vector<char> to2(int number) {   // перевод в 2-ную
     std::vector<char> bitNumber;
     if (number == 0) {
         bitNumber.resize(6, 0);
@@ -36,17 +30,15 @@ int to10(std::vector<char> number) {   // перевод из 2-ной
 }
 
 void writeNum(unsigned __int16 &numbers, std::vector<char> bitNumber,
-              int index) {   /// index  - индекс числа который нужно достать
-    for (int i = 0; i < 6; i++) {
-        if ((int)bitNumber[i] == 1) {
+              int index) {   /// index  - индекс число которое нужно достать(1-ое или 2-ое)
+    for (int i = 0; i < 6; i++)
+        if ((int) bitNumber[i] == 1)
             numbers |= (1 << (6 * index + i));
-
-        } else
+        else
             numbers &= ~(1 << (6 * index + i));
-    }
 }
 
-int readNum(unsigned __int16 &numbers, int index) {  //
+int readNum(unsigned __int16 &numbers, int index) {
     std::vector<char> bitNimbers;
     for (int i = 0; i < 6; i++) {
         if (numbers & (1 << (6 * index + i)))
@@ -57,10 +49,7 @@ int readNum(unsigned __int16 &numbers, int index) {  //
     }
     int result = to10(bitNimbers);
     return result;
-
-
 }
-
 
 int main() {  // макс размер памяти занимаемый числом равен 6 битами. Тогда нам потребуется 2 байта для хранения 2 чисел, т.е. 2 числа занимают 12 бит
     int number;
